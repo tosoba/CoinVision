ce2b0c0b6494a19fefe7d0cde203a099140d4b13
tosoba <therealmerengue@gmail.com>
2023-08-20 12:08:49 +0200

    switch to material 3

--- shared/src/commonMain/kotlin/com/trm/coinvision/App.kt
+++ shared/src/commonMain/kotlin/com/trm/coinvision/App.kt
@@ -1,13 +1,14 @@
 package com.trm.coinvision
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
@@ -16,14 +17,15 @@ import cafe.adriel.voyager.navigator.tab.TabNavigator
 import com.trm.coinvision.ui.compare.CompareTab
 import com.trm.coinvision.ui.list.ListTab
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
   MaterialTheme {
     TabNavigator(CompareTab) {
       Scaffold(
-        topBar = { TopAppBar { Text("CoinVision") } },
+        topBar = { CenterAlignedTopAppBar(title = { Text("CoinVision") }) },
         bottomBar = {
-          BottomNavigation {
+          NavigationBar {
             TabNavigationItem(CompareTab)
             TabNavigationItem(ListTab)
           }
@@ -38,7 +40,7 @@ fun App() {
 @Composable
 private fun RowScope.TabNavigationItem(tab: Tab) {
   val tabNavigator = LocalTabNavigator.current
-  BottomNavigationItem(
+  NavigationBarItem(
     selected = tabNavigator.current.key == tab.key,
     onClick = { tabNavigator.current = tab },
     icon = { tab.options.icon?.let { Icon(painter = it, contentDescription = tab.options.title) } },
--- shared/src/commonMain/kotlin/com/trm/coinvision/ui/list/ListTab.kt
+++ shared/src/commonMain/kotlin/com/trm/coinvision/ui/list/ListTab.kt
@@ -1,7 +1,7 @@
 package com.trm.coinvision.ui.list
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
