328f77803d355ba9eb5a67109ce90043d1d777de
tosoba <therealmerengue@gmail.com>
2023-10-28 21:21:51 +0200

    add necessary params to getCoinMarkets endpoint

--- shared/src/commonMain/kotlin/com/trm/coinvision/core/data/CryptoNetworkRepository.kt
+++ shared/src/commonMain/kotlin/com/trm/coinvision/core/data/CryptoNetworkRepository.kt
@@ -1,5 +1,6 @@
 package com.trm.coinvision.core.data
 
+import androidx.annotation.IntRange
 import com.trm.coinvision.core.data.mapper.isValid
 import com.trm.coinvision.core.data.mapper.toDomain
 import com.trm.coinvision.core.domain.model.CoinMarketsItem
@@ -16,12 +17,28 @@ class CryptoNetworkRepository(private val coinGeckoClient: HttpClient) : CryptoR
   private val FiatCurrency.queryParam: String
     get() = name.lowercase()
 
-  override suspend fun getCoinMarkets(vsFiatCurrency: FiatCurrency): List<CoinMarketsItem> =
+  override suspend fun getCoinMarkets(
+    vsFiatCurrency: FiatCurrency,
+    page: Int,
+    perPage: Int,
+    ids: List<String>?,
+    order: String,
+    sparkline: Boolean,
+    locale: String,
+    @IntRange(from = 0L, to = 18L) precision: Short
+  ): List<CoinMarketsItem> =
     coinGeckoClient
       .get(COIN_GECKO_API_BASE_URL) {
         url {
           appendPathSegments("coins", "markets")
           parameters.append("vs_currency", vsFiatCurrency.queryParam)
+          parameters.append("page", page.toString())
+          parameters.append("per_page", perPage.toString())
+          ids?.let { parameters.append("ids", it.joinToString(separator = ",")) }
+          parameters.append("order", order)
+          parameters.append("sparkline", sparkline.toString())
+          parameters.append("locale", locale)
+          parameters.append("precision", precision.toString())
         }
       }
       .body<List<CoinMarketsResponseItem>>()
--- shared/src/commonMain/kotlin/com/trm/coinvision/core/domain/repo/CryptoRepository.kt
+++ shared/src/commonMain/kotlin/com/trm/coinvision/core/domain/repo/CryptoRepository.kt
@@ -1,8 +1,18 @@
 package com.trm.coinvision.core.domain.repo
 
+import androidx.annotation.IntRange
 import com.trm.coinvision.core.domain.model.CoinMarketsItem
 import com.trm.coinvision.core.domain.model.FiatCurrency
 
 interface CryptoRepository {
-  suspend fun getCoinMarkets(vsFiatCurrency: FiatCurrency): List<CoinMarketsItem>
+  suspend fun getCoinMarkets(
+    vsFiatCurrency: FiatCurrency,
+    page: Int = 1,
+    perPage: Int = 250,
+    ids: List<String>? = null,
+    order: String = "market_cap_desc",
+    sparkline: Boolean = false,
+    locale: String = "en",
+    @IntRange(from = 0L, to = 18L) precision: Short = 2
+  ): List<CoinMarketsItem>
 }
