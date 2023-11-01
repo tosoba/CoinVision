7d47e027c6d68192efed7f6cb02e1deb41bdfb04
tosoba <therealmerengue@gmail.com>
2023-11-01 16:54:12 +0100

    remove useless wrapper box from TopSearchBar

--- shared/src/commonMain/kotlin/com/trm/coinvision/ui/TopSearchBar.kt
+++ shared/src/commonMain/kotlin/com/trm/coinvision/ui/TopSearchBar.kt
@@ -25,10 +25,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.isContainer
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,43 +34,38 @@ fun TopSearchBar() {
   var active by rememberSaveable { mutableStateOf(false) }
 
   Box(Modifier.fillMaxSize()) {
-    // Talkback focus order sorts based on x and y position before considering z-index. The
-    // extra Box with semantics and fillMaxWidth is a workaround to get the search bar to focus
-    // before the content.
-    Box(Modifier.semantics { isContainer = true }.zIndex(1f).fillMaxWidth()) {
-      DockedSearchBar(
-        modifier = Modifier.align(Alignment.TopCenter).padding(top = 8.dp),
-        query = text,
-        onQueryChange = { text = it },
-        onSearch = { active = false },
-        active = active,
-        onActiveChange = { active = it },
-        placeholder = { Text("Hinted search text") },
-        leadingIcon = { Icon(Icons.Rounded.Search, contentDescription = null) },
-        trailingIcon = { Icon(Icons.Rounded.MoreVert, contentDescription = null) },
+    DockedSearchBar(
+      modifier = Modifier.align(Alignment.TopCenter).padding(top = 8.dp),
+      query = text,
+      onQueryChange = { text = it },
+      onSearch = { active = false },
+      active = active,
+      onActiveChange = { active = it },
+      placeholder = { Text("Hinted search text") },
+      leadingIcon = { Icon(Icons.Rounded.Search, contentDescription = null) },
+      trailingIcon = { Icon(Icons.Rounded.MoreVert, contentDescription = null) },
+    ) {
+      LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
       ) {
-        LazyColumn(
-          modifier = Modifier.fillMaxWidth(),
-          contentPadding = PaddingValues(16.dp),
-          verticalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-          items(4) { idx ->
-            val resultText = "Suggestion $idx"
-            ListItem(
-              modifier =
-                Modifier.clickable {
-                  text = resultText
-                  active = false
-                },
-              headlineContent = {
-                Text(text = resultText, style = MaterialTheme.typography.titleMedium)
+        items(4) { idx ->
+          val resultText = "Suggestion $idx"
+          ListItem(
+            modifier =
+              Modifier.clickable {
+                text = resultText
+                active = false
               },
-              supportingContent = {
-                Text(text = "Additional info", style = MaterialTheme.typography.titleMedium)
-              },
-              leadingContent = { Icon(Icons.Rounded.Star, contentDescription = null) },
-            )
-          }
+            headlineContent = {
+              Text(text = resultText, style = MaterialTheme.typography.titleMedium)
+            },
+            supportingContent = {
+              Text(text = "Additional info", style = MaterialTheme.typography.titleMedium)
+            },
+            leadingContent = { Icon(Icons.Rounded.Star, contentDescription = null) },
+          )
         }
       }
     }
