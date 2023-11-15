8be1215398b01bedf60434a4943fe260a22f2482
Tomasz Osoba <tomasz.osoba@knowit.eu>
2023-11-15 17:14:54 +0100

    datastore path retrieval change on iOS - build still fails

--- shared/src/iosMain/kotlin/com/trm/coinvision/core/cache/HttpCacheStorage.kt
+++ shared/src/iosMain/kotlin/com/trm/coinvision/core/cache/HttpCacheStorage.kt
@@ -4,23 +4,24 @@ import com.trm.coinvision.core.cache.disk.DiskCacheStorage
 import io.ktor.client.plugins.cache.storage.*
 import io.ktor.client.plugins.cache.storage.CacheStorage
 import okio.FileSystem
-import okio.Path
 import okio.Path.Companion.toPath
 import platform.Foundation.NSCachesDirectory
 import platform.Foundation.NSSearchPathForDirectoriesInDomains
 import platform.Foundation.NSUserDomainMask
 
-private val cacheDir: Path =
-  NSSearchPathForDirectoriesInDomains(
-      directory = NSCachesDirectory,
-      domainMask = NSUserDomainMask,
-      expandTilde = true
-    )
-    .first()
-    .toString()
-    .toPath()
-
 internal actual class PlatformHttpCacheStorageInitializer : HttpCacheStorageInitializer {
   override fun invoke(maxSize: Long): CacheStorage =
-    DiskCacheStorage(fileSystem = FileSystem.SYSTEM, directory = cacheDir, maxSize = maxSize)
+    DiskCacheStorage(
+      fileSystem = FileSystem.SYSTEM,
+      directory =
+        NSSearchPathForDirectoriesInDomains(
+            directory = NSCachesDirectory,
+            domainMask = NSUserDomainMask,
+            expandTilde = true
+          )
+          .first()
+          .toString()
+          .toPath(),
+      maxSize = maxSize
+    )
 }
--- shared/src/iosMain/kotlin/com/trm/coinvision/core/datastore/PreferencesDatastore.kt
+++ shared/src/iosMain/kotlin/com/trm/coinvision/core/datastore/PreferencesDatastore.kt
@@ -3,24 +3,30 @@ package com.trm.coinvision.core.datastore
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.SupervisorJob
+import okio.Path.Companion.toPath
 import platform.Foundation.NSDocumentDirectory
-import platform.Foundation.NSFileManager
-import platform.Foundation.NSURL
+import platform.Foundation.NSSearchPathForDirectoriesInDomains
 import platform.Foundation.NSUserDomainMask
 
 internal actual class PlatformDatastoreInitializer : DatastoreInitializer {
   override fun invoke(): DataStore<Preferences> =
     PreferenceDataStoreFactory.createWithPath(
+      corruptionHandler = null,
+      migrations = emptyList(),
+      scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
       produceFile = {
-        val documentDirectory: NSURL? =
-          NSFileManager.defaultManager.URLForDirectory(
+        NSSearchPathForDirectoriesInDomains(
             directory = NSDocumentDirectory,
-            inDomain = NSUserDomainMask,
-            appropriateForURL = null,
-            create = false,
-            error = null,
+            domainMask = NSUserDomainMask,
+            expandTilde = true
           )
-        requireNotNull(documentDirectory).path + "/$DATASTORE_FILE_NAME"
+          .first()
+          .toString()
+          .toPath() / DATASTORE_FILE_NAME
       }
     )
 }
