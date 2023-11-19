6cfe689569f9a2c2e474577941ebd0c3036fb85b
tosoba <therealmerengue@gmail.com>
2023-11-19 14:04:49 +0100

    extract TokensLazyColumn out of TokensListTab

--- shared/src/commonMain/kotlin/com/trm/coinvision/ui/tokensList/TokensListTab.kt
+++ shared/src/commonMain/kotlin/com/trm/coinvision/ui/tokensList/TokensListTab.kt
@@ -18,12 +18,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
+import androidx.paging.PagingData
+import app.cash.paging.compose.LazyPagingItems
 import app.cash.paging.compose.collectAsLazyPagingItems
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import com.trm.coinvision.core.common.di.getScreenModel
 import com.trm.coinvision.core.common.util.LocalStringResources
 import com.trm.coinvision.core.common.util.LocalWidthSizeClass
+import com.trm.coinvision.core.domain.model.TokenListItemDTO
 import com.trm.coinvision.ui.common.CoinVisionProgressIndicator
 import com.trm.coinvision.ui.common.CoinVisionRetryColumn
 import com.trm.coinvision.ui.common.CoinVisionRetryRow
@@ -31,6 +34,7 @@ import com.trm.coinvision.ui.common.SelectedTokenData
 import com.trm.coinvision.ui.tokensSearchBar.TokensSearchBar
 import com.trm.coinvision.ui.tokensSearchBar.TokensSearchBarArgs
 import com.trm.coinvision.ui.tokensSearchBar.tokensSearchBarPadding
+import kotlinx.coroutines.flow.flowOf
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -39,21 +43,31 @@ internal class TokensListTab(private val mainSearchBarArgs: TokensSearchBarArgs)
   override fun Content() {
     val screenModel = getScreenModel<TokensListScreenModel>()
 
-    val token by screenModel.selectedToken.collectAsState()
     val listState = rememberLazyListState()
+    val tokens = screenModel.tokensPagingFlow.collectAsLazyPagingItems()
 
     if (LocalWidthSizeClass.current != WindowWidthSizeClass.Compact) {
+      val token by screenModel.selectedToken.collectAsState()
+
       Row(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.weight(.5f).fillMaxHeight()) {
           MainTokensSearchBar()
           SelectedTokenData(modifier = Modifier.fillMaxSize(), token = token)
         }
-        TokensLazyColumn(modifier = Modifier.weight(.5f).fillMaxHeight(), state = listState)
+        TokensLazyColumn(
+          modifier = Modifier.weight(.5f).fillMaxHeight(),
+          state = listState,
+          tokens = tokens
+        )
       }
     } else {
       Column(modifier = Modifier.fillMaxSize()) {
         MainTokensSearchBar()
-        TokensLazyColumn(modifier = Modifier.fillMaxSize(), state = listState)
+        TokensLazyColumn(
+          modifier = Modifier.fillMaxSize(),
+          state = listState,
+          tokens = tokens,
+        )
       }
     }
   }
@@ -66,52 +80,6 @@ internal class TokensListTab(private val mainSearchBarArgs: TokensSearchBarArgs)
     )
   }
 
-  @Composable
-  private fun TokensLazyColumn(modifier: Modifier = Modifier, state: LazyListState) {
-    val screenModel = getScreenModel<TokensListScreenModel>()
-    val tokens = screenModel.tokensPagingFlow.collectAsLazyPagingItems()
-    LazyColumn(modifier = modifier, contentPadding = PaddingValues(10.dp), state = state) {
-      if (tokens.loadState.prepend is LoadState.Error) {
-        item {
-          CoinVisionRetryRow(
-            modifier = Modifier.fillMaxWidth().padding(20.dp),
-            onRetryClick = tokens::retry
-          )
-        }
-      } else if (tokens.loadState.prepend == LoadState.Loading) {
-        item { CoinVisionProgressIndicator(modifier = Modifier.padding(20.dp)) }
-      }
-
-      when (tokens.loadState.refresh) {
-        is LoadState.Error -> {
-          item {
-            CoinVisionRetryColumn(
-              modifier = Modifier.fillParentMaxSize(),
-              onRetryClick = tokens::retry
-            )
-          }
-        }
-        LoadState.Loading -> {
-          item { CoinVisionProgressIndicator(modifier = Modifier.fillParentMaxSize()) }
-        }
-        is LoadState.NotLoading -> {
-          items(tokens.itemCount) { index -> tokens[index]?.let { Text(text = it.name) } }
-        }
-      }
-
-      if (tokens.loadState.append is LoadState.Error) {
-        item {
-          CoinVisionRetryRow(
-            modifier = Modifier.fillMaxWidth().padding(20.dp),
-            onRetryClick = tokens::retry
-          )
-        }
-      } else if (tokens.loadState.prepend == LoadState.Loading) {
-        item { CoinVisionProgressIndicator(modifier = Modifier.padding(20.dp)) }
-      }
-    }
-  }
-
   @OptIn(ExperimentalResourceApi::class)
   override val options: TabOptions
     @Composable
@@ -121,4 +89,53 @@ internal class TokensListTab(private val mainSearchBarArgs: TokensSearchBarArgs)
         title = LocalStringResources.current.list,
         icon = painterResource("list.xml")
       )
+}
+
+@Composable
+private fun TokensLazyColumn(
+  modifier: Modifier = Modifier,
+  state: LazyListState = rememberLazyListState(),
+  tokens: LazyPagingItems<TokenListItemDTO> =
+    flowOf(PagingData.empty<TokenListItemDTO>()).collectAsLazyPagingItems(),
+) {
+  LazyColumn(modifier = modifier, contentPadding = PaddingValues(10.dp), state = state) {
+    if (tokens.loadState.prepend is LoadState.Error) {
+      item {
+        CoinVisionRetryRow(
+          modifier = Modifier.fillMaxWidth().padding(20.dp),
+          onRetryClick = tokens::retry
+        )
+      }
+    } else if (tokens.loadState.prepend == LoadState.Loading) {
+      item { CoinVisionProgressIndicator(modifier = Modifier.padding(20.dp)) }
+    }
+
+    when (tokens.loadState.refresh) {
+      is LoadState.Error -> {
+        item {
+          CoinVisionRetryColumn(
+            modifier = Modifier.fillParentMaxSize(),
+            onRetryClick = tokens::retry
+          )
+        }
+      }
+      LoadState.Loading -> {
+        item { CoinVisionProgressIndicator(modifier = Modifier.fillParentMaxSize()) }
+      }
+      is LoadState.NotLoading -> {
+        items(tokens.itemCount) { index -> tokens[index]?.let { Text(text = it.name) } }
+      }
+    }
+
+    if (tokens.loadState.append is LoadState.Error) {
+      item {
+        CoinVisionRetryRow(
+          modifier = Modifier.fillMaxWidth().padding(20.dp),
+          onRetryClick = tokens::retry
+        )
+      }
+    } else if (tokens.loadState.prepend == LoadState.Loading) {
+      item { CoinVisionProgressIndicator(modifier = Modifier.padding(20.dp)) }
+    }
+  }
 }
