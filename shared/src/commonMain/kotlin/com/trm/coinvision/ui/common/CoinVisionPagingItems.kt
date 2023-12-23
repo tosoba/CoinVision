634e5b9d95959444036eb98a0429fd55b360a0ac
Tomasz Osoba <tomasz.osoba@knowit.eu>
2023-12-23 12:41:56 +0100

    add spacers to lazy list error views

--- shared/src/commonMain/kotlin/com/trm/coinvision/ui/common/CoinVisionPagingItems.kt
+++ shared/src/commonMain/kotlin/com/trm/coinvision/ui/common/CoinVisionPagingItems.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -11,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.trm.coinvision.core.common.util.LocalStringResources
 import com.trm.coinvision.core.domain.exception.HttpException
 import io.ktor.utils.io.errors.IOException
@@ -27,6 +31,7 @@ internal fun CoinVisionRetryRow(
     horizontalArrangement = Arrangement.Center
   ) {
     Text(text = text, maxLines = 2, overflow = TextOverflow.Ellipsis)
+    Spacer(modifier = Modifier.width(5.dp))
     Button(onRetryClick) { Text(text = LocalStringResources.current.retry) }
   }
 }
@@ -50,6 +55,7 @@ internal fun CoinVisionRetryColumn(
     horizontalAlignment = Alignment.CenterHorizontally
   ) {
     Text(text = text, maxLines = 2, overflow = TextOverflow.Ellipsis)
+    Spacer(modifier = Modifier.height(5.dp))
     Button(onRetryClick) { Text(text = LocalStringResources.current.retry) }
   }
 }
