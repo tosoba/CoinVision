e1ec46cb1a496d0fdec74c3f62a3b7b82af792ee
Tomasz Osoba <tomasz.osoba@knowit.eu>
2023-12-25 15:36:20 +0100

    fix toAreaData chart mapper (s vs ms was the problem)

--- shared/src/androidMain/kotlin/com/trm/coinvision/ui/chart/PriceChartMappersAndroid.kt
+++ shared/src/androidMain/kotlin/com/trm/coinvision/ui/chart/PriceChartMappersAndroid.kt
@@ -2,5 +2,7 @@ package com.trm.coinvision.ui.chart
 
 import com.tradingview.lightweightcharts.api.series.models.AreaData
 import com.tradingview.lightweightcharts.api.series.models.Time
+import java.util.Date
 
-internal fun PriceChartPoint.toAreaData() = AreaData(time = Time.Utc(timestamp), value = value)
+internal fun PriceChartPoint.toAreaData(): AreaData =
+  AreaData(time = Time.Utc.fromDate(Date(timestamp)), value = value)
