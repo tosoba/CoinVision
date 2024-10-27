0aecfbe7be679f52ac5c73e32c2fa8006d787195
merengue <therealmerengue@proton.me>
2024-10-27 11:46:06 +0100

    remove useless ui tests

--- shared/src/androidUnitTest/kotlin/com/trm/coinvision/androidTest.kt
+++
@@ -1,7 +0,0 @@
-package com.trm.coinvision
-
-import org.junit.Test
-
-class AndroidGreetingTest {
-  @Test fun testExample() {}
-}
--- shared/src/iosTest/kotlin/com/trm/coinvision/iosTest.kt
+++
@@ -1,7 +0,0 @@
-package com.trm.coinvision
-
-import kotlin.test.Test
-
-class IosGreetingTest {
-  @Test fun testExample() {}
-}
