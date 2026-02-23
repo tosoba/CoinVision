2f63c3d0960607306a37483079e432460e02fedd
merengue <therealmerengue@proton.me>
2026-02-23 16:58:06 +0100

    Simplify uppercase call in TokenSymbol

--- shared/src/commonMain/kotlin/com/trm/coinvision/ui/common/TokenSymbol.kt
+++ shared/src/commonMain/kotlin/com/trm/coinvision/ui/common/TokenSymbol.kt
@@ -1,6 +1,5 @@
 package com.trm.coinvision.ui.common
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Box
@@ -11,17 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.intl.LocaleList
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun TokenSymbol(symbol: String = "", modifier: Modifier = Modifier.tokenSymbolShape()) {
   Box(modifier = modifier) {
     Text(
       modifier = Modifier.align(Alignment.Center).basicMarquee(),
-      text = symbol.toUpperCase(LocaleList.current),
+      text = symbol.uppercase(),
       style = MaterialTheme.typography.labelMedium,
       color = MaterialTheme.colorScheme.onPrimary,
       maxLines = 1,
